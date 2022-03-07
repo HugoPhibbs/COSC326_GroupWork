3d4011a79fc64c95e0c19ae03e4f283c10ed53aa
Ben-forde <101066891+Ben-forde@users.noreply.github.com>
2022-03-07 22:01:11 +1300

    Add files via upload

--- src/SyllableSlam.java
+++ src/SyllableSlam.java
@@ -5,32 +5,43 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Class that predicts the number of syllables in a word
- */
+  * Class that predicts the number of syllables in a word
+  */
 public class SyllableSlam {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.print("Enter a word: ");
         String word = scanner.nextLine();
-        
-        System.out.println(word.charAt(6));
+        System.out.println(syllableCount(getSyllableArray(word)));
 
     }
 
-    public String[] getSyllableArray(String word) throws FileNotFoundException, IOException{
+    public static String[] getSyllableArray(String word) throws FileNotFoundException, IOException {
         String[] syllables = {};
-
-        try (BufferedReader reader = new BufferedReader(new FileReader("Syllables.txt"))){
-            for(String line; (line = reader.readLine()) != null; ) {
-                String [] wordArray = line.split("=");
-                if(wordArray[0].equals(word)){
-                    syllables = wordArray[1].split(" ");
-
+        try (BufferedReader reader = new BufferedReader(new FileReader("Syllables.txt"))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                String[] wordArray = line.split("=");
+                if (wordArray[0].equals(word)) {
+                    syllables = wordArray[1].split("�");
+                    return syllables;
                 }
+            }
+        } catch (Exception e){
+            System.out.println("cant find dataset");
+            
+            // will add the average method later which just get the average of words the same length.
         }
+        return syllables;
     }
-    return syllables;
-    } 
+
+    public static int syllableCount(String[] syllables) {
+        int count = 0;
+        for(int i = 0; i< syllables.length; i++ ){
+            count++;
+
+        }
+        return count;
+    }
+
 }
